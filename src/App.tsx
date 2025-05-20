a300fcb86091d5d8ff2fe731cde2d7227cd0258f
Pedro Ap <pedroaparecidori@gmail.com>
2025-05-20 15:13:18 -0300

    Ultimas alterações no app e garage

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,60 @@ import Navbar from './components/Navbar/Navbar'
 import Garage from './components/Garage/Garage';
 import Paragraphy from './components/Paragraphy/Paragraphy';
 import Footer from './components/Footer/Footer';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState<any[]>([])
 
-  const ws = new WebSocket('wss://rafeu.squareweb.app/parking/updates')
+  const wurl = 'wss://rafeu.squareweb.app/parking/updates'
 
-  ws.onmessage = (event) => {
-    const data = JSON.parse(event.data)
+  const ws = new WebSocket(wurl)
 
-    console.log('Atualização', data)
-  }
-  console.log(ws)
+  useEffect(() => {  
+
+    ws.onopen = () => {
+      console.log('conectado')
+    }
+
+    ws.onmessage = (event) => {
+      const data = JSON.parse(event.data)
+      logbest(data)
+      setMessage([data, data, data, data])
+    }
+
+    function logbest(data: any) {
+      console.log(data)
+    }
+
+    ws.onclose = () => {
+      console.log('Desconectado')
+    }
+
+    ws.onerror = (err: any) => {
+      console.log(err)
+    }
+
+    return () => {
+      if (ws && ws.readyState === WebSocket.OPEN)
+        ws.close()
+    }
+  }, [wurl])
+console.log(message)
   return (
     <div id='first'>
       <div id='second'>
         <Navbar />
       </div>
       <div id='third'>
-        <Garage boolean={false} />
-        <Garage boolean={true} />
-        <Garage boolean={true} />
-        <Garage boolean={true} />
-      </div>
-      <div id='fourth'> 
-        <Paragraphy>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, obcaecati eum fugiat atque recusandae voluptatem laborum sed autem nobis a, maxime repellat voluptate doloremque voluptates ipsam, consequuntur dolorum sapiente quidem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, quia. Autem similique reprehenderit explicabo eligendi deleniti dolore atque amet, pariatur nostrum aspernatur distinctio tempora? Molestias cum voluptatibus at mollitia ad. Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore voluptates dolorem expedita, tempore porro nobis, voluptatem illum sequi sapiente molestiae cumque neque quas iste in consequatur alias, facilis modi reprehenderit! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias nobis molestiae cum quasi. Ducimus numquam vero, reiciendis cupiditate ratione velit, nobis laudantium molestiae, dicta necessitatibus quidem magni consectetur saepe tempora! Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, temporibus magni? Molestias laborum magni ipsam dolore voluptatem. Sed illo ratione nesciunt atque rerum beatae impedit alias ipsum. Facere, ex placeat! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe aut quasi nesciunt sint eius ipsum temporibus. Voluptate ipsa eligendi perferendis doloribus libero? Culpa mollitia saepe commodi odit, unde recusandae cupiditate? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde ipsa odit quas fugit quasi, sunt sed assumenda sit iure omnis, suscipit totam nam ipsum repellat obcaecati a doloremque dolorum dolor. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius laboriosam hic pariatur. Cupiditate ducimus exercitationem aperiam corrupti, atque culpa, explicabo odio quibusdam quis optio velit dolorum necessitatibus fuga eos voluptatum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. At id doloremque nobis dolor facere quia nemo labore itaque aut ipsum, earum saepe vel commodi culpa error, aperiam ex consequatur tempore.</Paragraphy>
-        <Paragraphy>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, obcaecati eum fugiat atque recusandae voluptatem laborum sed autem nobis a, maxime repellat voluptate doloremque voluptates ipsam, consequuntur dolorum sapiente quidem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, quia. Autem similique reprehenderit explicabo eligendi deleniti dolore atque amet, pariatur nostrum aspernatur distinctio tempora? Molestias cum voluptatibus at mollitia ad. Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore voluptates dolorem expedita, tempore porro nobis, voluptatem illum sequi sapiente molestiae cumque neque quas iste in consequatur alias, facilis modi reprehenderit! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias nobis molestiae cum quasi. Ducimus numquam vero, reiciendis cupiditate ratione velit, nobis laudantium molestiae, dicta necessitatibus quidem magni consectetur saepe tempora! Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, temporibus magni? Molestias laborum magni ipsam dolore voluptatem. Sed illo ratione nesciunt atque rerum beatae impedit alias ipsum. Facere, ex placeat! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe aut quasi nesciunt sint eius ipsum temporibus. Voluptate ipsa eligendi perferendis doloribus libero? Culpa mollitia saepe commodi odit, unde recusandae cupiditate? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde ipsa odit quas fugit quasi, sunt sed assumenda sit iure omnis, suscipit totam nam ipsum repellat obcaecati a doloremque dolorum dolor. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius laboriosam hic pariatur. Cupiditate ducimus exercitationem aperiam corrupti, atque culpa, explicabo odio quibusdam quis optio velit dolorum necessitatibus fuga eos voluptatum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. At id doloremque nobis dolor facere quia nemo labore itaque aut ipsum, earum saepe vel commodi culpa error, aperiam ex consequatur tempore.</Paragraphy>
+        {
+          Array.isArray(message) ? (
+          message.map(index => (
+            <Garage key={index.spotId} boolean={index.isOccupied} name={index.spotId}/>
+          ))
+        ) : (
+          <p>Espere</p>
+        )
+        }
       </div>
       <Footer>Feito por Grupo xx - UNIVESP</Footer>
     </div>
--- src/components/Garage/Garage.tsx
+++ src/components/Garage/Garage.tsx
@@ -3,6 +3,7 @@ import './Garage.css'
 export default function Garage(props: any) {
     return(
         <div id='garage' style={ props.boolean ? { backgroundColor: '#20dfaf' } : { backgroundColor: '#df2050' } }>
+            Vaga: {props.name}
         </div>
     )
 }
